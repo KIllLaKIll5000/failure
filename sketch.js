0981b0c7a3fd98138cc2a2248127fdcadcf733c3
Cbarnes <cbarnes@wlapcs.org>
2017-04-11 16:46:02 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var newWidth = 100;
+var newHight = 100;
+
+
+
+
+function setup(){
+    createCanvas(600,400); 
+    background(50);
+}
+function draw(){
+    if(mouseIspressed){
+      noStroke();
+      fill(150,0,255,50)
+      ellipse(mouseX,mouseY,newHight,newWidth)
+        
+        
+}
+//i dont know why its not working, i sat here for an hour and the exact same thing you did but its not working and i dont know anymore then this
